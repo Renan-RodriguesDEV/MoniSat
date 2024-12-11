548113a7a5c7d9e7840a2c31758834090871086d
Renan-RodriguesDEV <renanrodrigues7110@gmail.com>
2024-12-11 15:04:59 -0300

    grid tá joia

--- __classes__/gridSat.py
+++ __classes__/gridSat.py
@@ -60,22 +60,23 @@ class MoniSAT:
         try:
             app_data_path = os.getenv("LOCALAPPDATA")
             user_data_path = os.path.join(
-                app_data_path, r"Google\Chrome\User Data\Default\Bot"
+                app_data_path, r"Google\Chrome\User Data\Default\Google Profile"
             )
 
+            logger.info(f"AppData: {app_data_path}")
             # Criar diretório para screenshots se não existir
             os.makedirs(os.path.join(os.getcwd(), "cards"), exist_ok=True)
 
             # Iniciar o playwright
             self.playwright = sync_playwright().start()
-            self.browser = self.playwright.chromium.launch()
-            # self.browser = self.playwright.chromium.launch_persistent_context(
-            #     user_data_path,
-            #     headless=False,
-            #     args=['--start-maximized'],
-            #     no_viewport=True,
-            #     user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 Edg/116.0.1938.81'
-            # )
+            # self.browser = self.playwright.chromium.launch()
+            self.browser = self.playwright.chromium.launch_persistent_context(
+                user_data_path,
+                headless=False,
+                args=["--start-maximized"],
+                no_viewport=True,
+                user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 Edg/116.0.1938.81",
+            )
 
             self.url = "https://site.monisat.com.br/index.php"
             logger.debug("Classe iniciada com sucesso.")
@@ -84,7 +85,7 @@ class MoniSAT:
             self.fechar()
             raise
 
-    def extrator(self):
+    def extrator_to_grid(self, map=True, list_results: bool | dict = True):
         try:
             logger.debug("Iniciando extração.")
             self.page_monisat = self.browser.new_page()
@@ -117,28 +118,82 @@ class MoniSAT:
             self.page_monisat.wait_for_load_state("networkidle")
             logger.info("Login efetuado.")
 
-            logger.info('Extraindo informações dos elementos div[class="card"].')
-            cards = self.page_monisat.query_selector_all('div[class="card"]')
+            # Navegar até o grid
+            grid_element = "#navigation > ul > li:nth-child(4) > a"
+            full_screen_element = "#mapaGrid > div.leaflet-control-container > div.leaflet-top.leaflet-left > div.leaflet-control-fullscreen.leaflet-bar.leaflet-control > a"
+            grid_map_element = "#mapaGrid"
 
-            for index, card in enumerate(cards):
-                card_body = card.query_selector('div[class="card-body"]')
-                if card_body:
-                    h4_element = card_body.query_selector("h4")
+            logger.info(f"Extraindo informações dos elementos {grid_element}")
+            self.page_monisat.click(grid_element)
+            self.page_monisat.click(full_screen_element)
+            grid_map = self.page_monisat.locator(grid_map_element)
+            screenshot_path = os.path.join(os.getcwd(), "cards", f"MAP.png")
+            self.page_monisat.wait_for_load_state("networkidle")
+            grid_map.screenshot(path=screenshot_path)
+            logger.info(f"Screenshot saved as {screenshot_path}")
+            self.page_monisat.click(
+                "#mapaGrid > div.leaflet-control-container > div.leaflet-top.leaflet-left > div.leaflet-control-fullscreen.leaflet-bar.leaflet-control > a"
+            )
 
-                if h4_element:
-                    card_info = h4_element.inner_text()
-                    logger.info(f"Card {index + 1}: {card_info}")
-
-                    screenshot_path = os.path.join(
-                        os.getcwd(), "cards", f'{card_info.replace(".", "")}.png'
-                    )
-                    card.screenshot(path=screenshot_path)
-                    logger.info(f"Screenshot saved as {screenshot_path}")
+            # Verificação da lista
+            if list_results is True:  # Apenas tira o screenshot da lista completa
+                logger.info("Capturando tela da lista sem filtros...")
+                self.__extract_to_list()
+            elif isinstance(
+                list_results, dict
+            ):  # Preenche os campos e tira o screenshot
+                self.__extract_to_list(**list_results)
 
         except Exception as e:
             logger.error(f"Erro durante a extração: {str(e)}")
             self.fechar()
             raise
+
+    def __extract_to_list(self, **kwargs):
+        elements = {
+            "placa": "#GGPLACA > center > input",
+            "carreta": "#GGSEMIREBOQUE > center > input",
+            "reboque": "#select2-SGGSITUACAOVEICULO4-container",
+            "st_veiculo": "#select2-SGGSITUACAOVEICULO-container",
+            "moto": "#GGMOTORISTA > center > input",
+            "posicao": "#GGPOSICAO > center > input",
+            "loc": "#GGLOCALIZACAO > center > input",
+            "rota": "#GGROTA > center > input",
+            "st_viagem": "#GGSITUACAOVIAGEM > center > input",
+            "st_viagem_per": "#GGSITUACAOVIAGEMDET > center > input",
+            "temp_viagem": "#GGTEMPORESTANTEVIAGEM > center > input",
+            "st_geral": "#GGSITUACAOGERAL > center > input",
+            "km": "#GGKMRESTANTEVIAGEM > center > input",
+            "operacao": "#GGOPERACAO > center > input",
+            "tt_parado": "#GGSTOTALPARADO > center > input",
+            "sensor_1": "#GGTEMPSENSOR1 > center > input",
+            "acao": "#GGACAO > center > input",
+            "fx_1": "#GGTEMPFAIXA1 > center > input",
+            "bau": "#GGSPORTABAU > center > input",
+            "checklist": "#GGSCHECLIST > center > input",
+            "velocidade": "#GGVELOCIDADE > center > input",
+        }
+
+        if kwargs:
+            # Preenche os campos se houver parâmetros
+            for key, value in kwargs.items():
+                if key in elements:
+                    self.page_monisat.fill(selector=elements[key], value=value)
+                else:
+                    logger.warning(
+                        f"Parâmetro '{key}' não reconhecido e será ignorado."
+                    )
+        else:
+            logger.info("Nenhum filtro fornecido. Capturando lista completa...")
+
+        # Captura de tela da lista
+        div_list_element = ".dataTables_scroll"
+        div_list = self.page_monisat.locator(div_list_element)
+        if div_list:
+            self.page_monisat.pause()
+            screenshot_path = os.path.join(os.getcwd(), "cards", f"LISTA_GRID.png")
+            div_list.screenshot(path=screenshot_path)
+            logger.info("Screenshot saved as LISTA_GRID.png")
 
     def fechar(self):
         try:
@@ -152,9 +207,10 @@ class MoniSAT:
 
 
 if __name__ == "__main__":
+    handler = MoniSAT()
+    parametros = {}
     try:
-        handler = MoniSAT()
-        handler.extrator()
+        handler.extrator_to_grid(list_results=True)
     except Exception as e:
         logger.error(f"Erro na execução do script: {str(e)}")
     finally:
